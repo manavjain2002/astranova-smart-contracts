e5c9a3c739d36dae91a283653364d6509c81f6f0
manavjain2002 <manavj4254@gmail.com>
2024-01-27 09:34:22 +0530

    feat: hardhat setup with env example

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@openzeppelin/hardhat-upgrades");
+require('dotenv').config()
 
-const config: HardhatUserConfig = {
+module.exports = {
   solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
-
-export default config;
